421cd838d4cd31a22a306a936dab5fbc50bef314
OfentseLearnsCode <malebyeoc@gmail.com>
2023-06-26 08:56:25 +0200

    Initial Commit

---
+++ ProgrammaticUIkit2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIkit2
+//
+//  Created by DA MAC M1 137 on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
